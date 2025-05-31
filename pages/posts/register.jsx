76a168f18ddde491d3860cd09644e82897236511
Arayik <arayik.asatryan10@gmail.com>
2025-05-31 11:49:00 +0400

    hot fix

--- pages/posts/register.jsx
+++ pages/posts/register.jsx
@@ -1,36 +1,34 @@
-import AutoLayout from "@/components/Layouts/autoLayout";
+import AutoLayout from "../../components/Layouts/autoLayout";
 import { useRouter } from "next/router";
 import React, { useState } from "react";
 import { Label } from "recharts";
 
 const Register = () => {
-  const [userName, setUserName] = useState('');
-  const [userEmail, setUserEmail] = useState('');
-  const [userPassword, setUserPassword] = useState('')
+  const [userName, setUserName] = useState("");
+  const [userEmail, setUserEmail] = useState("");
+  const [userPassword, setUserPassword] = useState("");
 
-  const router = useRouter()
+  const router = useRouter();
 
-  const handlerAction = async (e)=>{
-    e.preventDefault()
-    router.push('/')
+  const handlerAction = async (e) => {
+    e.preventDefault();
+    router.push("/");
     try {
-        await fetch("http://localhost:3001/register", {
-          method: "post",
-          headers: {
-            "content-type": "application/json",
-          },
-          body: JSON.stringify({
-            name: userName,
-            email: userEmail,
-            password: userPassword
-
-          }),
-        });
-      
-      } catch (error) {
-        console.log(error);
-      }
-  }
+      await fetch("http://localhost:3001/register", {
+        method: "post",
+        headers: {
+          "content-type": "application/json",
+        },
+        body: JSON.stringify({
+          name: userName,
+          email: userEmail,
+          password: userPassword,
+        }),
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  };
   return (
     <div>
       <form action="#" method="POST" onSubmit={handlerAction}>
